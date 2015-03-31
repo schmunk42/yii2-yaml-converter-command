53e599b232817896d85898c128a588d769bd5b3e
Tobias Munk <schmunk@usrbin.de>
2015-03-31 21:14:01 +0200

    added public properties and yaml dependecy

--- YamlController.php
+++ YamlController.php
@@ -12,14 +12,17 @@ namespace dmstr\console\controllers;
 use Symfony\Component\Yaml\Yaml;
 use yii\console\Controller;
 use yii\helpers\ArrayHelper;
-use yii\helpers\Console;
 
 class YamlController extends Controller
 {
 
+    public $dockerComposeFile = '@app/docker-compose.yml';
+    public $templateDirectory = '@app/build';
+    public $outputDirectory = '@app/build/generated-stacks';
+
     public function actionConvert()
     {
-        $file   = file_get_contents(\Yii::getAlias('@app/../docker-compose.yml'));
+        $file      = file_get_contents(\Yii::getAlias($this->dockerComposeFile));
         $stacks[0] = Yaml::parse($file);
 
         foreach ($stacks[0] as $i => $services) {
@@ -38,16 +41,16 @@ class YamlController extends Controller
             }
         }
 
-        $file   = file_get_contents(\Yii::getAlias('@app/../stacks/templates/test.yml'));
+        $file      = file_get_contents(\Yii::getAlias($this->templateDirectory.'/test.yml'));
         $stacks[1] = Yaml::parse($file);
-        $stacks = ArrayHelper::merge($stacks[0], $stacks[1]);
-        $data   = Yaml::dump($stacks, 2, 4, true, true);
+        $stacks    = ArrayHelper::merge($stacks[0], $stacks[1]);
+        $data      = Yaml::dump($stacks, 2, 4, true, true);
         file_put_contents(\Yii::getAlias('@app/../stacks/test.yml'), $data);
 
-        $file   = file_get_contents(\Yii::getAlias('@app/../stacks/templates/staging.yml'));
+        $file      = file_get_contents(\Yii::getAlias($this->templateDirectory.'/staging.yml'));
         $stacks[2] = Yaml::parse($file);
-        $stacks = ArrayHelper::merge($stacks, $stacks[2]);
-        $data   = Yaml::dump($stacks, 2, 4, true, true);
+        $stacks    = ArrayHelper::merge($stacks, $stacks[2]);
+        $data      = Yaml::dump($stacks, 2, 4, true, true);
         file_put_contents(\Yii::getAlias('@app/../stacks/staging.yml'), $data);
 
         $this->stdout("Done.\n");
