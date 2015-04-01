c3904386fbc7f8fc7e6b9ce971b99a47a611e92f
Tobias Munk <schmunk@usrbin.de>
2015-04-01 04:30:14 +0200

    updated parsing (WIP)

--- YamlController.php
+++ YamlController.php
@@ -18,19 +18,25 @@ class YamlController extends Controller
 
     public $dockerComposeFile = '@app/docker-compose.yml';
     public $templateDirectory = '@app/build';
-    public $outputDirectory = '@app/build/generated-stacks';
+    public $outputDirectory = '@app/build/stacks-generated';
+    public $stacks = ['test', 'ci', 'staging', 'production'];
 
     public function actionConvert()
     {
         $file      = file_get_contents(\Yii::getAlias($this->dockerComposeFile));
-        $stacks[0] = Yaml::parse($file);
+        $base      = Yaml::parse($file);
 
-        foreach ($stacks[0] as $i => $services) {
+        $file   = file_get_contents(\Yii::getAlias($this->templateDirectory . '/test.yml'));
+        $stack  = Yaml::parse($file);
+        $stacks = ArrayHelper::merge($base, $stack);
+        $data   = Yaml::dump($stacks, 10);
+        file_put_contents(\Yii::getAlias($this->outputDirectory . '/test.yml'), $data);
+
+        foreach ($stacks as $i => $services) {
             foreach ($services as $j => $service) {
                 #var_dump($stacks[0][$i]);
-                unset($stacks[0][$i]['volumes']);
-                unset($stacks[0][$i]['build']);
-
+                unset($stacks[$i]['volumes']);
+                unset($stacks[$i]['build']);
                 ###$stacks[0][$i]['tags'] = ['app'];
             }
             if ($i == 'ftp') {
@@ -41,17 +47,17 @@ class YamlController extends Controller
             }
         }
 
-        $file      = file_get_contents(\Yii::getAlias($this->templateDirectory.'/test.yml'));
-        $stacks[1] = Yaml::parse($file);
-        $stacks    = ArrayHelper::merge($stacks[0], $stacks[1]);
-        $data      = Yaml::dump($stacks, 2, 4, true, true);
-        file_put_contents(\Yii::getAlias('@app/../stacks/test.yml'), $data);
+        $file   = file_get_contents(\Yii::getAlias($this->templateDirectory . '/ci.yml'));
+        $stack  = Yaml::parse($file);
+        $stacks = ArrayHelper::merge($stacks, $stack);
+        $data   = Yaml::dump($stacks, 10);
+        file_put_contents(\Yii::getAlias($this->outputDirectory . '/ci.yml'), $data);
 
-        $file      = file_get_contents(\Yii::getAlias($this->templateDirectory.'/staging.yml'));
-        $stacks[2] = Yaml::parse($file);
-        $stacks    = ArrayHelper::merge($stacks, $stacks[2]);
-        $data      = Yaml::dump($stacks, 2, 4, true, true);
-        file_put_contents(\Yii::getAlias('@app/../stacks/staging.yml'), $data);
+        $file   = file_get_contents(\Yii::getAlias($this->templateDirectory . '/staging.yml'));
+        $stack  = Yaml::parse($file);
+        $stacks = ArrayHelper::merge($stacks, $stack);
+        $data   = Yaml::dump($stacks, 10);
+        file_put_contents(\Yii::getAlias($this->outputDirectory . '/staging.yml'), $data);
 
         $this->stdout("Done.\n");
     }
